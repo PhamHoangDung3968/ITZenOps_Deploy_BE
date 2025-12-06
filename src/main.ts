e9e7211c92b6634ffc2354f9d368901d1a82574a
PhamHoangDung <phamhoangdung3968@gmail.com>
2025-12-06 11:05:07 +0700

    test deploy

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,31 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   app.enableCors();
+//   await app.listen(process.env.PORT ?? 3000);
+// }
+// bootstrap();
+
+
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+
+  // ✅ Bật CORS với cấu hình chi tiết
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',        // cho dev
+      'http://localhost:3001',        // nếu FE chạy cổng khác
+      'https://itzenops.vercel.app', // domain FE khi deploy
+    ],
+    credentials: true, // cho phép gửi cookie, Authorization header
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
